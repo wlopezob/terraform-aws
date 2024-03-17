23d65df35d10d3a4d1898131c18a0e38f4546e9b
DESKTOP-CSBAJF8\WLOPEZOB <wlopezob@gmail.com>
2024-03-17 16:42:18 -0500

    add app-database-v1

---
+++ app/app-database/src/main/java/com/wlopezob/appdatabase/AppDatabaseApplication.java
@@ -0,0 +1,13 @@
+package com.wlopezob.appdatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppDatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppDatabaseApplication.class, args);
+	}
+
+}
---
+++ app/app-database/src/test/java/com/wlopezob/appdatabase/AppDatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wlopezob.appdatabase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppDatabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
